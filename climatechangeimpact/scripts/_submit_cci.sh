786d8f5a69be6c5d639f17cbed012bb38b488d2c
MarkMelotto <mmelotto@tudelft.nl>
2026-04-23 15:15:08 +0200

    testing the new destinE script and reverting to 5 year periods as well as updating scripts

---
+++ climatechangeimpact/scripts/_submit_cci.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+for country in regions/*
+do
+    country_name=$(basename "$country")
+
+    for region in "$country"/*
+    do
+        region_name=$(basename "$region")
+
+        sbatch \
+            --job-name="$region_name" \
+            --error="regions/$country_name/$region_name/$region_name.err" \
+            --output="regions/$country_name/$region_name/$region_name.out" \
+            --export=REGION_ID="$region_name",COUNTRY="$country_name" \
+            scripts/run_cci.slurm
+
+#        break 2  # done as testing to see if this works
+#       break 1  # done as testing to see if this works
+#        sleep 1  # for small workloads use this
+    done
+    sleep 25  # lets wait per region to improve workflow
+done
